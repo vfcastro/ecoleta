a248d3447a266675c9acbf978d641349f8c945f2
Vinicius Fraga de Castro <vinicius_decastro@vinicius>
2020-07-03 19:51:43 -0300

    start maps API integration

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi/';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 import './styles.css';
 
@@ -60,6 +61,12 @@ const CreatePoint = () => {
             <h2>Endereço</h2>
             <span>Selecione o endereço no mapa</span>            
           </legend>
+
+          <Map center={[-29.9395229,-51.1899153]} zoom={15}>
+            <TileLayer 
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          </Map>
 
           <div className="field-group">
             <div className="field">
