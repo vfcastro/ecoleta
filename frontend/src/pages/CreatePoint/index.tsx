98a912b517c6849d4f241ddd32a51cc7364a2098
Vinicius Fraga de Castro <vinicius_decastro@vinicius>
2020-07-16 23:00:29 -0300

    Handle map click event. Set map initial position.

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, ChangeEvent } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi/';
 import { Map, TileLayer, Marker } from 'react-leaflet';
+import { LeafletMouseEvent } from 'leaflet';
 import axios from 'axios';
 import api from '../../services/api';
 
@@ -27,9 +28,22 @@ const CreatePoint = () => {
 
   const [items, setItems] = useState<Item[]>([]);
   const [ufs, setUfs] = useState<string[]>([]);
+  const [cities, setCities] = useState<string[]>([]);
+
+  const [initialPosition, setInitialPosition] = useState<[number, number]>([-30.0277, -51.2287]);
+  
   const [selectedUf, setSelectedUf] = useState<string>('0');
   const [selectedCity, setSelectedCity] = useState<string>('0');
-  const [cities, setCities] = useState<string[]>([]);
+  const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0,0]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(position => {
+        const { latitude, longitude } = position.coords;
+      
+        setInitialPosition([latitude, longitude]);
+      }
+    );
+  }, []);
 
   useEffect(() => {
     api.get('items').then( response => {
@@ -67,6 +81,13 @@ const CreatePoint = () => {
   function handleSelectCity(event: ChangeEvent<HTMLSelectElement>){
     const city = event.target.value;
     setSelectedCity(city);
+  }
+
+  function handleMapClick(event: LeafletMouseEvent) {
+    setSelectedPosition([
+      event.latlng.lat,
+      event.latlng.lng,
+    ]);
   }
 
   return (
@@ -123,12 +144,12 @@ const CreatePoint = () => {
             <span>Selecione o endereço no mapa</span>            
           </legend>
 
-          <Map center={[-29.9395229,-51.1899153]} zoom={15}>
+          <Map center={initialPosition} zoom={15} onClick={handleMapClick}>
             <TileLayer 
               attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-            <Marker position={[-29.9395229,-51.1899153]}/>
+            <Marker position={selectedPosition} />
           </Map>
 
           <div className="field-group">
